fae0dba94eb97848027f67cfed945e38d6f941f0
Hikari Sakamoto <hikarisakamoto@gmail.com>
2017-06-13 11:45:49 -0300

    - Block Breaker finished - Updated other games to Unity 5 - Added Roll The Ball game

---
+++ Roll The Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    private void LateUpdate()// Best for follow cameras, procidural animations and get last known states -> It will run after all itens have been 
+                             // processed in Update
+    {
+        transform.position = player.transform.position + offset;
+    }
+}

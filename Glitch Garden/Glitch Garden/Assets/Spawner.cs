7d80e98cfd9e6d6f96912e41f4ffb72bc5d84fa6
Hikari Sakamoto <hikarisakamoto@gmail.com>
2017-07-07 15:46:35 -0300

    Quick update

---
+++ Glitch Garden/Glitch Garden/Assets/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+	public GameObject[] attackerPrefabArray;
+
+	void Update ()
+	{
+		foreach (GameObject thisAttacker in attackerPrefabArray) {
+			if (isTimeToSpawn (thisAttacker)) {
+				Spawn (thisAttacker);
+			}
+		}
+	}
+
+	void Spawn (GameObject myGameObject)
+	{
+		GameObject attacker = Instantiate (myGameObject) as GameObject;
+		attacker.transform.parent = transform;
+		attacker.transform.position = transform.position;
+	}
+
+	bool isTimeToSpawn (GameObject attackerGameObject)
+	{
+		return true;
+	}
+}
